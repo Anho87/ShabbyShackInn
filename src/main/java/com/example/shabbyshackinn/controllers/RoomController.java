af0bee1c0caba318a8fec49bb662d2e95e59234e
FelixDahlberg <felixdah@hotmail.com>
2024-05-01 13:57:09 +0200

    saving commit small changes to search booking

--- src/main/java/com/example/shabbyshackinn/controllers/RoomController.java
+++ src/main/java/com/example/shabbyshackinn/controllers/RoomController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -23,11 +24,15 @@ public class RoomController {
 
     @RequestMapping("/search")
     public String search(Model model, @RequestParam LocalDate startDate,
-                         @RequestParam LocalDate endDate, @RequestParam int amountOfPersons){
+                         @RequestParam LocalDate endDate, @RequestParam int numberOfGuests, RedirectAttributes redirectAttributes){
+        if (startDate == null || endDate == null || numberOfGuests >= 0){
+            redirectAttributes.addFlashAttribute("feedback", "All search fields must be filled");
+            return "redirect:/shabbyShackInn/index";
+        }
         model.addAttribute("startDate", startDate);
         model.addAttribute("endDate", endDate);
-        model.addAttribute("amountOfPersons", amountOfPersons);
-        List<DetailedRoomDto> availableRooms = roomService.findAvailableRooms(startDate,endDate,amountOfPersons);
+        model.addAttribute("numberOfGuests", numberOfGuests);
+        List<DetailedRoomDto> availableRooms = roomService.findAvailableRooms(startDate,endDate,numberOfGuests);
         model.addAttribute("searchResults", availableRooms);
         return "searchResults";
     }
