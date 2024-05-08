bfa854496f6c456422b4ecf946a79a7643930c9a
JohanJohnsson <johan.johnsson@yh.nackademin.se>
2024-05-08 14:53:46 +0200

    MiniContractCustomerDto created

--- src/main/java/com/example/shabbyshackinn/dtos/MiniContractCustomerDto.java
+++ src/main/java/com/example/shabbyshackinn/dtos/MiniContractCustomerDto.java
@@ -1,7 +1,5 @@
 package com.example.shabbyshackinn.dtos;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,8 +10,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class MiniContractCustomerDto {
-    @Id
-    @GeneratedValue
     public int id;
     public String companyName;
     public String contactName;
