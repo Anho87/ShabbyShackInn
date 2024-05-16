9e2f5aaa6d7954fc6ac573e58bd6870329ced83e
FelixDahlberg <felixdah@hotmail.com>
2024-05-16 10:17:40 +0200

    fixes related to blacklist

---
+++ src/main/java/com/example/shabbyshackinn/controllers/BlacklistController.java
@@ -0,0 +1,44 @@
+package com.example.shabbyshackinn.controllers;
+
+
+import com.example.shabbyshackinn.models.BlackListedCustomer;
+import com.example.shabbyshackinn.services.BlacklistService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+@RequestMapping("/shabbyShackInn")
+public class BlacklistController {
+
+    private final BlacklistService blacklistService;
+
+    public BlacklistController(BlacklistService blacklistService) {
+        this.blacklistService = blacklistService;
+    }
+
+    @RequestMapping("/manageBlacklist")
+    public String manageBlacklist(Model model){
+
+        return "manageBlacklist";
+    }
+
+    @PostMapping("/updateBlacklist")
+    public String updateBlacklist(@RequestParam String updateFirstName, @RequestParam String updateLastName,
+                                      @RequestParam String updateEmail, @RequestParam boolean okInBlacklist, RedirectAttributes redirectAttributes) {
+        BlackListedCustomer blackListedCustomer = BlackListedCustomer.builder().name(updateFirstName + " " + updateLastName).email(updateEmail).ok(okInBlacklist).group("shabbyShackInn").build();
+        blacklistService.updateBlacklistedCustomer(blackListedCustomer);
+        return "redirect:/shabbyShackInn/index";
+    }
+    @PostMapping("/addToBlacklist")
+    public String addToBlacklist(@RequestParam String addFirstName, @RequestParam String addLastName,
+                                 @RequestParam String addEmail, @RequestParam boolean okInBlacklist, RedirectAttributes redirectAttributes){
+        BlackListedCustomer blackListedCustomer = BlackListedCustomer.builder().name(addFirstName + " " + addLastName).email(addEmail).ok(okInBlacklist).group("shabbyShackInn").build();
+        blacklistService.addBlackListedCustomer(blackListedCustomer);
+        return "redirect:/shabbyShackInn/index";
+    }
+}
