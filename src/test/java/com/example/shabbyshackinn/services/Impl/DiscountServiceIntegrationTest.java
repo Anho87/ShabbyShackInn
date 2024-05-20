f3026a65861a1ebb24547cfbab827d54271d2ff5
Kalle Johansson <kalle.johansson@hotmail.com>
2024-05-20 14:54:36 +0200

    Updated DiscountServiceIntegrationTest

--- src/test/java/com/example/shabbyshackinn/services/Impl/DiscountServiceIntegrationTest.java
+++ src/test/java/com/example/shabbyshackinn/services/Impl/DiscountServiceIntegrationTest.java
@@ -13,7 +13,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.time.LocalDate;
-import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -29,21 +28,21 @@ public class DiscountServiceIntegrationTest {
 
     @BeforeEach
     void setUp() {
-        Customer customer = new Customer(1L, "John", "Doe", "123456789", "john.doe@example.com");
-        Customer customer2 = new Customer(2L, "John", "Doe", "123456789", "john.doe@example.com");
+        Customer customer = new Customer(1L, "John", "Doe", "111111111", "john.doe@example.com");
+        Customer customer2 = new Customer(2L, "Alex", "Johnson", "222222222", "alex.johnson@example.com");
         Room room = new Room(1L, RoomType.DOUBLE, 1, 2, 2000, 1);
 
         customerRepo.save(customer);
         customerRepo.save(customer2);
         roomRepo.save(room);
 
-        LocalDate startDate1 = LocalDate.now().minusDays(20);
-        LocalDate endDate1 = LocalDate.now().minusDays(10);
-        Booking booking1 = new Booking(1L, startDate1, endDate1, 123, 1, 999999999, customer, room);
+        LocalDate startDate1 = LocalDate.now().minusDays(10);
+        LocalDate endDate1 = LocalDate.now().minusDays(1);
+        Booking booking1 = new Booking(1L, startDate1, endDate1, 123, 1, 18000, customer, room);
 
-        LocalDate startDate2 = LocalDate.now().minusDays(50);
-        LocalDate endDate2 = LocalDate.now().minusDays(40);
-        Booking booking2 = new Booking(2L, startDate2, endDate2, 124, 1, 999999999, customer, room);
+        LocalDate startDate2 = LocalDate.now().minusDays(365);
+        LocalDate endDate2 = LocalDate.now().minusDays(354);
+        Booking booking2 = new Booking(2L, startDate2, endDate2, 124, 1, 22000, customer, room);
 
         bookingRepo.save(booking1);
         bookingRepo.save(booking2);
@@ -54,13 +53,10 @@ public class DiscountServiceIntegrationTest {
         LocalDate today = LocalDate.now();
         LocalDate oneYearAgo = today.minusYears(1);
 
-        Optional<Integer> nightsBooked20 = bookingRepo.sumNightsByCustomerIdAndYear(1L, oneYearAgo, today);
-        Optional<Integer> nightsBooked0 = bookingRepo.sumNightsByCustomerIdAndYear(2L, oneYearAgo, today);
+        int expecting20NightsBookedLastYear = bookingRepo.sumNightsByCustomerIdAndYear(1L, oneYearAgo, today).orElse(0) ;
+        int expecting0NightsBookedLastYear = bookingRepo.sumNightsByCustomerIdAndYear(2L, oneYearAgo, today).orElse(0);
 
-        int nightsBookedLastYear20 = nightsBooked20.orElse(0);
-        int nightsBookedLastYear0 = nightsBooked0.orElse(0);
-
-        assertEquals(20, nightsBookedLastYear20);
-        assertEquals(0, nightsBookedLastYear0);
+        assertEquals(20, expecting20NightsBookedLastYear);
+        assertEquals(0, expecting0NightsBookedLastYear);
     }
 }
