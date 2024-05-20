5adbca8acd2cdc10bcf8cc46af461be7c1e5ceb2
JohanJohnsson <johan.johnsson@yh.nackademin.se>
2024-05-20 09:10:35 +0200

    Integrationstest ShippersServiceImplIntegrationTest

--- src/test/java/com/example/shabbyshackinn/services/Impl/ShippersServiceImplIntegrationTest.java
+++ src/test/java/com/example/shabbyshackinn/services/Impl/ShippersServiceImplIntegrationTest.java
@@ -45,6 +45,31 @@ public class ShippersServiceImplIntegrationTest {
         assertEquals(3, shipper3.getId());
         assertEquals("Karlsson Gruppen", shipper3.getCompanyName());
         assertEquals("076-869-7192", shipper3.getPhone());
+
+        Shippers shipper4 = shippersList.get(3);
+        assertEquals(4, shipper3.getId());
+        assertEquals("Nilsson Kommanditbolag", shipper4.getCompanyName());
+        assertEquals("070-661-8894", shipper4.getPhone());
+
+        Shippers shipper5 = shippersList.get(4);
+        assertEquals(5, shipper3.getId());
+        assertEquals("Åslund-Gustafsson", shipper5.getCompanyName());
+        assertEquals("076-552-6784", shipper5.getPhone());
+
+        Shippers shipper6 = shippersList.get(5);
+        assertEquals(6, shipper3.getId());
+        assertEquals("Östlund, Änglund Group", shipper6.getCompanyName());
+        assertEquals("070-992-7785", shipper6.getPhone());
+
+        Shippers shipper7 = shippersList.get(6);
+        assertEquals(7, shipper3.getId());
+        assertEquals("Åslund Kommanditbolag", shipper7.getCompanyName());
+        assertEquals("070-656-1853", shipper7.getPhone());
+
+        Shippers shipper8 = shippersList.get(7);
+        assertEquals(8, shipper3.getId());
+        assertEquals("Johansson-Änglund", shipper8.getCompanyName());
+        assertEquals("070-136-6555", shipper8.getPhone());  
     }
 
 //    @Test
