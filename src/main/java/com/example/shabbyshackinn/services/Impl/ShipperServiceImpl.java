e8a13b934c66a1b5bd1b4bb474f8b685189da46d
JohanJohnsson <johan.johnsson@yh.nackademin.se>
2024-05-21 14:22:38 +0200

    Fixed ShipperServiceImpl

--- src/main/java/com/example/shabbyshackinn/services/Impl/ShipperServiceImpl.java
+++ src/main/java/com/example/shabbyshackinn/services/Impl/ShipperServiceImpl.java
@@ -17,7 +17,6 @@ import java.util.List;
 public class ShipperServiceImpl implements ShipperService {
 
     private final ObjectMapper mapper = new JsonMapper();
-    private final String apiUrl = "https://javaintegration.systementor.se/shippers";
     
     @Autowired
     public ShipperServiceImpl(JsonStreamProvider jsonStreamProvider, ShipperRepo shipperRepo) {
@@ -25,38 +24,16 @@ public class ShipperServiceImpl implements ShipperService {
         this.shipperRepo = shipperRepo;
     }
 
-    private final ShipperRepo shipperRepo;
+    final ShipperRepo shipperRepo;
     final JsonStreamProvider jsonStreamProvider;
-
-    @Override
-    public List<Shippers> getAllShippers() {
-        return shipperRepo.findAll();
-    }
     
-    @Override
-    public String addShipper(Shippers shipper) {
-        shipperRepo.save(shipper);
-        return "Shipper " + shipper.companyName + " is saved";
-    }
+    
 
     @Override
     public List<Shippers> getShippers() throws IOException {
         return jsonStreamProvider.getDataStreamShippersAsList();
     }
-
-    @Override
-    public void fetchAndSaveShippers() throws IOException {
-        for(Shippers shippers : getAllShippers()){
-            Shippers s = shipperRepo.findShippersById(shippers.id);
-            if(s == null){
-                s = (new Shippers());
-            }
-            s.id = shippers.id;
-            s.companyName = shippers.companyName;
-            s.phone = shippers.phone;
-            shipperRepo.save(s);
-        }
-    }
+    
 
     @Override
     public List<Shippers> getShippersFromAPI() {
