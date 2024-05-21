68a03eadf8fa71b6ef59d2893ce9c7884b175523
JohanJohnsson <johan.johnsson@yh.nackademin.se>
2024-05-21 10:34:57 +0200

    Changed name on ShippersServiceImplIntegrationTest.java

--- src/test/java/com/example/shabbyshackinn/services/Impl/ShippersServiceImplIntegrationTest.java
+++ src/test/java/com/example/shabbyshackinn/services/Impl/ShippersServiceImplIntegrationTest.java
@@ -14,7 +14,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
-public class ShipperServiceImplIntegrationTest {
+public class ShippersServiceImplIntegrationTest {
     @Autowired
     ShipperRepo shipperRepo;
 
