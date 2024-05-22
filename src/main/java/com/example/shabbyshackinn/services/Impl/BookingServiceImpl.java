b8aefc379d59e5dbd650117981f0f96cdd9671a1
Kalle Johansson <kalle.johansson@hotmail.com>
2024-05-22 14:14:06 +0200

    Fixed checkIfBookingPossible() test. Added more mocks to serve different bookingId possibilities

--- src/main/java/com/example/shabbyshackinn/services/Impl/BookingServiceImpl.java
+++ src/main/java/com/example/shabbyshackinn/services/Impl/BookingServiceImpl.java
@@ -132,22 +132,9 @@ public class BookingServiceImpl implements BookingService {
 
     @Override
     public boolean checkIfBookingPossible(DetailedBookingDto booking) {
-//        Long roomId = booking.getMiniRoomDto().getId();
-//        LocalDate startDate = booking.getStartDate();
-//        LocalDate endDate = booking.getEndDate();
-//        Long currentBookingId = booking.getId();
-//
-//        List<Booking> overlappingBookings = bookingRepo.findAll()
-//                .stream()
-//                .filter(b -> !b.getId().equals(currentBookingId)) 
-//                .filter(b -> b.getRoom().getId().equals(roomId))  
-//                .filter(b -> b.getStartDate().isBefore(endDate) && b.getEndDate().isAfter(startDate))
-//                .toList();
-//
-//        return overlappingBookings.isEmpty();
         List<Booking> overlappingBookings = bookingRepo.findAllByIdIsNotAndRoomIdAndStartDateIsBeforeAndEndDateIsAfter(booking.getId(), booking.getMiniRoomDto().getId(), booking.getEndDate(), booking.getStartDate());
-        System.out.println("New Booking = ID: " + booking.getId() + "|" + booking.getMiniRoomDto().getId() + "|" + booking.getStartDate() + "|" + booking.getEndDate());
-        overlappingBookings.forEach(b -> System.out.println("Old bookings = ID: " + b.getId() + "|" + b.getRoom().getId() + "|" + b.getStartDate() + "|" + b.getEndDate()));
+        //System.out.println("New Booking = ID: " + booking.getId() + "|" + booking.getMiniRoomDto().getId() + "|" + booking.getStartDate() + "|" + booking.getEndDate());
+        //overlappingBookings.forEach(b -> System.out.println("Old bookings = ID: " + b.getId() + "|" + b.getRoom().getId() + "|" + b.getStartDate() + "|" + b.getEndDate()));
         return overlappingBookings.isEmpty();
     }
 
