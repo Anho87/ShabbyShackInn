859050a28d7f2e1e0014a7fe7ecf0aa65c02803f
JohanJohnsson <johan.johnsson@yh.nackademin.se>
2024-05-27 11:31:54 +0200

    MailCRUD

--- src/main/java/com/example/shabbyshackinn/controllers/UserController.java
+++ src/main/java/com/example/shabbyshackinn/controllers/UserController.java
@@ -1,24 +1,28 @@
 package com.example.shabbyshackinn.controllers;
 
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
     
-    @GetMapping(path = "/manageUsers")
-    @PreAuthorize("hasAuthority('Admin')")
-    public String empty(Model model) {
-        model.addAttribute("manageUsers", "Admin");
-        return "admin";
-    }
+//    @GetMapping(path = "/manageUsers")
+//    @PreAuthorize("hasAuthority('Admin')")
+//    public String empty(Model model) {
+//        model.addAttribute("manageUsers", "Admin");
+//        return "admin";
+//    }
+//    
+//    @GetMapping(path = "/addUser")
+//    @PreAuthorize("hasAuthority('Admin')")
+//    public String addUser(Model model) {
+//        model.addAttribute("add", "Admin");
+//        return "index";
+//    }
     
-    @GetMapping(path = "/addUser")
-    @PreAuthorize("hasAuthority('Admin')")
-    public String addUser(Model model) {
-        model.addAttribute("add", "Admin");
-        return "index";
-    }
+//    @GetMapping
+//    @PreAuthorize("hasAuthority('Admin')")
+//    public String changeEmailLayout(Model model) {
+//        
+//    }
+    
 }
